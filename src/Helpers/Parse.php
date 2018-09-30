71af87a4f3397acfc53f42cc5beded24cd817f77
weiwenhao <1101140857@qq.com>
2018-10-01 01:33:15 +0800

    增加params解析

--- src/Helpers/Parse.php
+++ src/Helpers/Parse.php
@@ -9,11 +9,13 @@ trait Parse
         static $offset = 0;
         $array = [];
         $temp = [];
-        $string .= ',';
+        !$begin && $string .= ",";
 
-        while (isset($string[$offset]) && $char = $string[$offset++]) {
-            if (in_array($char, ['.', ',', '(', ')', '{', '}'], true)) {
-                $temp && $temp = $this->toString($temp);
+        while (isset($string[$offset])) {
+            $char = $string[$offset++];
+
+            if (in_array($char, ['.', ',', '(', ')', '{', '}', ';'], true)) {
+                is_array($temp) && $temp = $this->toString($temp);
 
                 if (in_array($char, ['.', '{'])) {
                     $array[$temp] = array_merge(
@@ -23,17 +25,27 @@ trait Parse
                 } elseif ($char === '(') {
 
                     $array[$temp]['params'] = $this->parseInclude($string, $char);
+                    // 接下来需要继续用到temp,因此不走到下面的清空temp部分
+                    if (in_array($string[$offset], ['.', '{'])) {
+                        continue;
+                    }
 
-                } elseif ($char === '}' || ($char === ',' && $begin === '.')) {
-                    $array[] = $temp;
+                } elseif (($char === '}' || $char === ',') && $begin === '.') {
+                    $temp && $array[] = $temp;
+                    // offset 使用了预偏移的模式, 所以这里要 - 2
+                    $offset = $offset - 1;
+
+                    return $array;
+                } elseif ($char === '}') {
+                    $temp && $array[] = $temp;
                     return $array;
 
                 } elseif ($char === ')') {
-                    $array[] = $temp;
+                    $temp && $array[] = $temp;
                     $array = $this->handleParams($array);
                     return $array;
                 } elseif ($char === ',') {
-                    $array[] = $temp;
+                    $temp && $array[] = $temp;
                 }
 
                 $temp = [];
